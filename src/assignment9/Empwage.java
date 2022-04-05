e7f8d300855146c125eaa1ca4dfff06dd760d51a
classroom <himanshukanoje23@gmail.com>
2022-04-06 01:56:39 +0530

    my commit

--- src/assignment9/Empwage.java
+++ src/assignment9/Empwage.java
@@ -14,11 +14,13 @@ import java.util.Scanner;
 		if(a==1)
 		{
 			System.out.println("yes the employee is present");
+			System.out.println("***************************************************");
 			
 			}
 		else 
 		{
 			System.out.println("the employee is absent");
+			System.out.println("***************************************************");
 		}
 
 		}
@@ -28,6 +30,19 @@ import java.util.Scanner;
 		int wage=20;
 	int	total=wage*hour;
 	System.out.println("the daily wage of the employee is\n"  +total);
+	System.out.println("***************************************************");
+	}
+	public void monthlywage()
+	{
+		Scanner s = new Scanner(System.in);
+	      System.out.println("please enter number of  working days in a month");
+	    int month  = s.nextInt();	
+	    System.out.println("please enter  wages per day");
+	    int days  = s.nextInt();	
+		
+		int total=month*days;
+		 System.out.println("the total wage for a given month is "+total);
+		 System.out.println("***************************************************");
 	}
 	
 	public void partemp()
@@ -38,6 +53,7 @@ import java.util.Scanner;
 		int hour=8;
 		int total=wage *hour;
 		 System.out.println(total);
+		 System.out.println("***************************************************");
 	}
 }	
  public class Empwage {
@@ -49,7 +65,7 @@ import java.util.Scanner;
 		a.calculate();
 		a.dailywage();
 		a.partemp();
-			
+		a.monthlywage();	
 
 		}
 
